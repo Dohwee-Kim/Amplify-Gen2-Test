a55cb29c365e83daaccac25fc5dc1908ed2d690a
Leo Kim <cloudkim2024@gmail.com>
2022-05-31 07:45:05 -0400

    TS commit -2

---
+++ TS/LD2/index.ts
@@ -0,0 +1,13 @@
+let 이름 :string = 'kim';
+let 나이 :number = 50;
+let 메리드 : boolean = false;
+let 진짜로 : undefined = undefined;
+
+//string 형을 받는 array 
+let 회원들:string[] = ['kim', 'park']
+
+//이런식으로 오브젝트에도 지정가능합니다. 타입을 제한
+let 오브젝트1:{userName: string, userLast: string} = { userName : 'kim', userLast : 'park'  }
+
+//온갖 곳에 다 하지말고 
+let 회원들2 = 'park' //기본적인 타입 지정 원래 자동으로 됩니다. 
